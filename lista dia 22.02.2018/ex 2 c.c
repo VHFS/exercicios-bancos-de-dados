04394663826c0da97d9c6a0bf6d6e16cda3a3c40
VHFS <36747415+VHFS@users.noreply.github.com>
2018-02-22 20:07:13 -0300

    Add files via upload

---
+++ lista dia 22.02.2018/ex 2 c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main( ) {
+
+ int x, *p; 
+ x=10; 
+ // Só se usa asterisco quando se está declaando o ponteiro, deve-se usar & para impimir valor contido em alguma variavel
+ p = &x; 
+ return 0 ;
+ }
+ 
