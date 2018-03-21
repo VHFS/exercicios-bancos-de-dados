ce4371a1f4a7d72b886ddcb6409c33bcc49bbd08
VHFS <36747415+VHFS@users.noreply.github.com>
2018-03-21 19:31:33 -0300

    Add files via upload

---
+++ lista dia 28.02.2018/exercicio.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+/*Faça um programa que leia um valor inteiro n e crie dinamicamente um vetor de n elementos
+do tipo inteiro. Em seguida o programa deve ler os elementos desse vetor. Depois, o vetor
+preenchido deve ser impresso. Além disso, antes de finalizar o programa, deve-se liberar a área
+de memória alocada.*/
+	
+	
+	int n = 0, *p;
+	int cont = 0;
+	
+	printf("Qual o tamanho do vetor:\n", n);
+	scanf("%i",&n);
+	
+
+	p=(int*)malloc(sizeof(int)*n);
+	
+	printf("Vetor de %i espacos criado.\n", n);
+
+	printf("Quais os elementos do vetor.\n", n);	
+	for(cont; cont < n; cont++){
+		printf("", n);	
+		scanf("%i",	p+cont );	
+	}
+	
+	
+	for(cont=0; cont < n; cont++){
+		printf("\nVetor %i: ", cont);
+		printf("%i\n",*(p+cont));	
+	}
+	
+	free(p);
+	return 0;
+}
